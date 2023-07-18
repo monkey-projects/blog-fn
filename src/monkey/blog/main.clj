8093fe8777d5d523d4b4721fc7ff22596703edbd
Wout Neirynck <wout@monkey-projects.be>
2023-07-18 10:07:45 +0200

    Added reitit and httpkit

--- src/monkey/blog/main.clj
+++ src/monkey/blog/main.clj
@@ -1,6 +1,22 @@
 (ns monkey.blog.main
   (:gen-class)
-  (:require [clojure.tools.logging :as log]))
+  (:require [clojure.tools.logging :as log]
+            [org.httpkit.server :as http]
+            [reitit
+             [core :as rc]
+             [ring :as rr]]))
+
+(defn- health [_]
+  {:status 200})
+
+(def router
+  (rr/router
+   [["/health" {:name ::health
+                :get health}]]))
+
+(def handler (rr/ring-handler router))
 
 (defn -main [& args]
-  (log/info "Main goes here"))
+  (let [opts {:port 8080}]
+    (log/info "Starting HTTP server at port" (:port opts))
+    (http/run-server router opts)))
--- test/monkey/blog/test/main_test.clj
+++ test/monkey/blog/test/main_test.clj
@@ -1,8 +1,18 @@
 (ns monkey.blog.test.main-test
   (:require [clojure.test :refer :all]
-            [monkey.blog.main :as sut]))
+            [monkey.blog.main :as sut]
+            [org.httpkit.server :as http]))
+
+(deftest handler
+  (testing "is a fn"
+    (is (fn? sut/handler)))
+
+  (testing "GET /health"
+    (is (= 200 (-> (sut/handler {:request-method :get
+                                 :uri "/health"})
+                   :status)))))
 
 (deftest -main
-  (testing "does nothing yet"
-    (is (nil? (sut/-main)))))
-
+  (testing "starts http server"
+    (with-redefs-fn {#'http/run-server (constantly :test-server)}
+      #(is (= :test-server (sut/-main))))))
