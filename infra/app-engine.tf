2984574caccf291483c0502dab7a1ec16634564f
Wout Neirynck <wout@monkey-projects.be>
2023-07-22 11:36:31 +0200

    Added custom domain

--- infra/app-engine.tf
+++ infra/app-engine.tf
@@ -2,3 +2,10 @@ resource "google_app_engine_application" "app" {
   project     = var.project_id
   location_id = var.location_id
 }
+
+resource "google_app_engine_domain_mapping" "test_domain" {
+  domain_name = "test.neirynck.org"
+  ssl_settings {
+    ssl_management_type = "AUTOMATIC"
+  }
+}
