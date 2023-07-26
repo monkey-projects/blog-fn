51c1bd5c793e607f70a188315971b021397a99a5
Wout Neirynck <wout@monkey-projects.be>
2023-07-26 17:45:25 +0200

    Additional test tags handling

--- src/test-frontend/monkey/blog/fe/test/junit_reporter.cljs
+++ src/test-frontend/monkey/blog/fe/test/junit_reporter.cljs
@@ -8,10 +8,15 @@
 
 (defmethod update-report :default [ctx]
   ;; Noop
+  (printerr "Unsupported test tag type:" (:type ctx))
+  (printerr "All:" ctx)
   ctx)
 
 (defn- add-out [ctx msg]
   (assoc ctx ::out msg))
+
+(defn- update-state [ctx f & args]
+  (apply update ctx ::state f args))
 
 (defmethod update-report :begin-run-tests [ctx]
   (add-out ctx "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<testsuites>"))
@@ -20,13 +25,39 @@
   (add-out ctx "</testsuites>"))
 
 (defmethod update-report :begin-test-ns [ctx]
-  (add-out ctx "<testsuite>"))
+  (-> ctx
+      (add-out "<testsuite>")
+      (update-state assoc :ns (:ns ctx))))
 
 (defmethod update-report :end-test-ns [ctx]
-  (add-out ctx "</testsuite>"))
+  (-> ctx
+      (add-out "</testsuite>")
+      (update-state dissoc :ns)))
+
+(defmethod update-report :begin-test-var [{:keys [var] :as ctx}]
+  (printerr "Begin var:" var)
+  (assoc-in ctx [::state :var] var))
+
+(defmethod update-report :end-test-var [{:keys [var] :as ctx}]
+  (printerr "End var:" var)
+  #_(update-in ctx [::state :var] conj var)
+  (update-state ctx dissoc :var))
+
+(defmethod update-report :pass [ctx]
+  (update-state ctx update :pass (fnil inc 0)))
+
+(defmethod update-report :fail [ctx]
+  (update-state ctx update :fail (fnil inc 0)))
+
+(defmethod update-report :error [ctx]
+  (update-state ctx update :error (fnil inc 0)))
+
+(defmethod update-report :summary [{:keys [pass fail error test] :as ctx}]
+  (println "Test executed:" test "total," pass "passed," fail "failed," error "error(s)")
+  ctx)
 
 (defn reporter [state {:keys [type] :as ctx}]
-  (printerr "Type:" type ", keys:" (keys ctx))
+  #_(printerr "Type:" type ", keys:" (keys ctx))
   ;; Not for parallel execution, but that's ok in node
   (let [{:keys [::out] new-state ::state :as new-ctx} (update-report (assoc ctx ::state @state))]
     (reset! state new-state)
--- src/test-frontend/monkey/blog/fe/test/junit_reporter_test.cljs
+++ src/test-frontend/monkey/blog/fe/test/junit_reporter_test.cljs
@@ -22,10 +22,67 @@
     (let [r (sut/update-report {:type :begin-test-ns
                                 :ns "test.ns"})]
       (testing "writes `<testsuite>` tag"
-        (is (cs/includes? (::sut/out r) "<testsuite>")))))
+        (is (cs/includes? (::sut/out r) "<testsuite>")))
+
+      (testing "sets ns in state"
+        (is (= "test.ns" (-> r ::sut/state :ns))))))
 
   (testing "end-test-ns"
     (let [r (sut/update-report {:type :end-test-ns
-                                :ns "test.ns"})]
+                                :ns "test.ns"
+                                ::sut/state {:ns "test.ns"
+                                             :vars [:a :b]}})]
       (testing "writes `<testsuite>` end tag"
-        (is (cs/includes? (::sut/out r) "</testsuite>"))))))
+        (is (cs/includes? (::sut/out r) "</testsuite>")))
+
+      (testing "clears ns from state"
+        (is (nil? (-> r ::sut/state :ns))))))
+
+  (testing "begin-test-var"
+    (let [r (sut/update-report {:type :begin-test-var
+                                :var "test var"})]
+      (testing "sets var in state"
+        (is (= "test var" (-> r ::sut/state :var))))))
+
+  (testing "end-test-var"
+    (let [r (sut/update-report {:type :end-test-var
+                                :var "test var"
+                                ::sut/state {:var "test var"}})]
+      (testing "clears var from state"
+        (is (nil? (-> r ::sut/state :var))))))
+
+  (testing "pass"
+    (testing "sets pass in state"
+      (is (= 1 (-> (sut/update-report {:type :pass})
+                   ::sut/state
+                   :pass))))
+      
+    (testing "increases pass in state"
+      (is (= 2 (-> (sut/update-report {:type :pass
+                                       ::sut/state {:pass 1}})
+                   ::sut/state
+                   :pass)))))
+
+  (testing "fail"
+    (testing "sets fail in state"
+      (is (= 1 (-> (sut/update-report {:type :fail})
+                   ::sut/state
+                   :fail))))
+      
+    (testing "increases fail in state"
+      (is (= 2 (-> (sut/update-report {:type :fail
+                                       ::sut/state {:fail 1}})
+                   ::sut/state
+                   :fail)))))
+
+  (testing "error"
+    (testing "sets error in state"
+      (is (= 1 (-> (sut/update-report {:type :error})
+                   ::sut/state
+                   :error))))
+      
+    (testing "increases error in state"
+      (is (= 2 (-> (sut/update-report {:type :error
+                                       ::sut/state {:error 1}})
+                   ::sut/state
+                   :error))))))
